0cd7e77a57a30bc2ccc5f717da436764c721d154
Ondřej Švarc <ondrej.svarc@czechitas.cz>
2025-05-12 16:07:27 +0200

    Update WebSocketConfig.java

--- src/main/java/com/spshpau/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/spshpau/chatservice/config/WebSocketConfig.java
@@ -40,7 +40,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "http://192.168.1.121:5173")
                 .withSockJS();
     }
 
